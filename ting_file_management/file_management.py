0f68daf06891883e3599d17c43930f56d0587901
MATHEUS CAITANO BATISTA <matheuscb@msn.com>
2024-04-12 20:00:57 -0300

    feat(req02): Implementa função txt_importer para importar notícias de arquivos TXT
    Este commit adiciona a função txt_importer ao módulo file_management no pacote ting_file_management. A função é capaz de importar notícias a partir de um arquivo TXT, utilizando "/n" como separador de linhas.
    
    Funcionalidades implementadas:
    - A função lê o conteúdo do arquivo especificado e retorna as linhas como uma lista.
    - Caso o arquivo não seja encontrado, exibe a mensagem de erro "Arquivo {path_file} não encontrado" na stderr.
    - Caso a extensão do arquivo seja diferente de .txt, exibe a mensagem de erro "Formato inválido" na stderr.
    
    As mudanças garantem que o programa seja capaz de importar notícias de arquivos TXT com sucesso e lidar adequadamente com situações de erro.

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,14 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    try:
+        with open(path_file, 'r') as file:
+            if not path_file.endswith('.txt'):
+                raise ValueError("Formato inválido")
+            lines = [line.strip() for line in file.readlines()]
+            return lines
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+    except ValueError as e:
+        print(e, file=sys.stderr)
