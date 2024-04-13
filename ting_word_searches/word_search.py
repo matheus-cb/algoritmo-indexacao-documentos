7cbe4230bbf4cc7fd727492f37d4bbb45dcff6a1
MATHEUS CAITANO BATISTA <matheuscb@msn.com>
2024-04-13 14:16:47 -0300

    feat(req07 e 08): Implementação das funções exists_word e search_by_word
    Foram adicionadas as funções exists_word e search_by_word ao módulo word_search do pacote ting_word_searches.
    
    A função exists_word verifica a existência de uma palavra em todos os arquivos processados, retornando uma lista com as informações de cada arquivo e suas linhas onde a palavra foi encontrada.
    
    Já a função search_by_word busca uma palavra em todos os arquivos processados, retornando uma lista semelhante à de exists_word, mas incluindo também o conteúdo das linhas onde a palavra foi encontrada. Ambas as funções respeitam os critérios de busca case insensitive e não alteram a fila original durante a busca.
    
    Os testes correspondentes a essas funções foram implementados e estão passando corretamente.

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,41 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    occurrences = []
+
+    for i in range(len(instance)):
+        file_data = instance.search(i)
+        file_name = file_data["nome_do_arquivo"]
+        lines = file_data["linhas_do_arquivo"]
+        file_occurrences = {
+            "palavra": word, "arquivo": file_name, "ocorrencias": []
+        }
+
+        for line_number, line in enumerate(lines, start=1):
+            if word.lower() in line.lower():
+                file_occurrences["ocorrencias"].append({"linha": line_number})
+
+        if file_occurrences["ocorrencias"]:
+            occurrences.append(file_occurrences)
+
+    return occurrences
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    occurrences = []
+
+    for i in range(len(instance)):
+        file_data = instance.search(i)
+        file_name = file_data["nome_do_arquivo"]
+        lines = file_data["linhas_do_arquivo"]
+        file_occurrences = {
+            "palavra": word, "arquivo": file_name, "ocorrencias": []
+        }
+
+        for line_number, line_content in enumerate(lines, start=1):
+            if word.lower() in line_content.lower():
+                occurrence = {"linha": line_number, "conteudo": line_content}
+                file_occurrences["ocorrencias"].append(occurrence)
+
+        if file_occurrences["ocorrencias"]:
+            occurrences.append(file_occurrences)
+
+    return occurrences
