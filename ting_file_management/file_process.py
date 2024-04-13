c1a4265695d7f0dae57e7a99ba7bd0552e41aa06
MATHEUS CAITANO BATISTA <matheuscb@msn.com>
2024-04-13 10:20:42 -0300

    feat(req03, 04 e 05): Implementação dos requisitos 3, 4 e 5
    Requisito 3: Implementada a função process para processar arquivos e adicionar metadados à fila. A função verifica se o arquivo já foi processado antes de adicioná-lo à fila e exibe os dados processados via stdout.
    
    Requisito 4: Implementada a função remove para remover o primeiro arquivo processado da fila. A função exibe uma mensagem via stdout informando se o arquivo foi removido com sucesso ou se não há elementos na fila.
    
    Requisito 5: Implementada a função file_metadata para apresentar as informações superficiais de um arquivo processado. A função busca os metadados do arquivo na posição especificada na fila e os exibe via stdout. Se a posição for inválida, uma mensagem de erro é impressa via stderr.

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,10 +1,39 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for i in range(len(instance)):
+        if instance.search(i)["nome_do_arquivo"] == path_file:
+            print(
+               f"Arquivo {path_file} já processado. Ignorando...",
+               file=sys.stdout
+            )
+            return
+    lines = txt_importer(path_file)
+    file_data = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(lines),
+            "linhas_do_arquivo": lines
+    }
+    instance.enqueue(file_data)
+    print(file_data, file=sys.stdout)
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    try:
+        removed_file = instance.dequeue()
+        print(
+            f"Arquivo {removed_file['nome_do_arquivo']} removido com sucesso",
+            file=sys.stdout
+        )
+    except IndexError:
+        print("Não há elementos", file=sys.stdout)
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    try:
+        file_data = instance.search(position)
+        print(file_data, file=sys.stdout)
+    except IndexError:
+        print("Posição inválida", file=sys.stderr)
